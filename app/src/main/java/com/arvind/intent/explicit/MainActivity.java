e4a21eabf567ae3dd7a0826dc94ab96c13318d20
Arvind Iyer <arvindi.me@gmail.com>
2016-02-27 04:00:56 +0800

    Return data from activity on close using extras

--- app/src/main/java/com/arvind/intent/explicit/MainActivity.java
+++ app/src/main/java/com/arvind/intent/explicit/MainActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    private static final int REQUEST_CODE = 10;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,13 +20,24 @@ public class MainActivity extends Activity {
         EditText text = (EditText) findViewById(R.id.inputforintent);
         // used later
         String value = text.getText().toString();
-        // TODO 1 create new Intent(context, class)
         // use the activity as context parameter
         // and "ResultActivity.class" for the class parameter
         Intent intent = new Intent(this, ResultActivity.class);
-        // TODO 2 start second activity with
-        // startActivity(intent);
-        startActivity(intent);
+        intent.putExtra("value", value);
+
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data.hasExtra("returnvalue")) {
+                String result = data.getExtras().getString("returnvalue");
+                if (result != null && result.length() > 0) {
+                    Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/arvind/intent/explicit/ResultActivity.java
+++ app/src/main/java/com/arvind/intent/explicit/ResultActivity.java
@@ -1,15 +1,36 @@
 package com.arvind.intent.explicit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * Created by arvind on 2/27/16.
- */
+
 public class ResultActivity extends Activity {
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+        Bundle extras = getIntent().getExtras();
+        String value = extras.getString("value");
+        System.out.println(value);
         setContentView(R.layout.activity_result);
+        TextView textView = (TextView) findViewById(R.id.displayintentextra);
+        textView.setText(value);
+    }
+
+    @Override
+    public void finish() {
+        //Create new intent
+        Intent intent = new Intent();
+        //Read EditText
+        EditText editText = (EditText) findViewById(R.id.returnvalue);
+        String extra = editText.getText().toString();
+        //Add extra to intent
+        intent.putExtra("returnvalue", extra);
+        //Set result
+        setResult(RESULT_OK, intent);
+
+        super.finish();
     }
 }
