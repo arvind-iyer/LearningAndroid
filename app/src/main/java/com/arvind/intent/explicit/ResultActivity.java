676db62eb2fa2fb3bd99efd048916492cd3a6862
Arvind Iyer <arvindi.me@gmail.com>
2016-02-27 03:47:20 +0800

    Project using intents and sending data between them

---
+++ app/src/main/java/com/arvind/intent/explicit/ResultActivity.java
@@ -0,0 +1,15 @@
+package com.arvind.intent.explicit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by arvind on 2/27/16.
+ */
+public class ResultActivity extends Activity {
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_result);
+    }
+}
